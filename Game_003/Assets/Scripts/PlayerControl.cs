43bc947ffaa4fe78bad38aba2fb661728c71bf7b
Unknown <xvictor.victor@gmail.com>
2018-02-26 22:26:58 +0100

    Interaction and fixes
    Interaction is not completed.
    Fixed player being through ground and added a red ball that is going to be interactable.

--- Game_003/Assets/Scripts/PlayerControl.cs
+++ Game_003/Assets/Scripts/PlayerControl.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(PlayerMotor))]
+
+
 public class PlayerControl : MonoBehaviour {
+
     public LayerMask movementMask;
+    public Interactable focus;
 
     Camera cam;
     PlayerMotor motor;
@@ -14,34 +18,54 @@ public class PlayerControl : MonoBehaviour {
         motor = GetComponent<PlayerMotor>(); 
 	}
 	
+    //Når man venstreklikker
 	void Update () {
-		if (Input.GetMouseButtonDown(1))
+		if (Input.GetMouseButtonDown(0))
         {
+            //får man en ray
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
+            //og hvis den ray rammer noget
             if (Physics.Raycast(ray, out hit, 100, movementMask))
             {
-                //Flytter spiller til hvor der blev klikket
+                //så flytter spilleren til hvad vi ramte.
                 motor.MoveToPoint(hit.point);
 
                 //Stopper med at fokusere et objekt
+                RemoveFocus();
 
             }
         }
-        if (Input.GetMouseButtonDown(0))
+
+        //når man højreklikkerz
+        if (Input.GetMouseButtonDown(1))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
+
+            // Hvis ray rammer
             if (Physics.Raycast(ray, out hit, 100, movementMask))
             {
-                Debug.Log("Hello motherfucking " + hit.collider.name + " " + hit.point);
-
-                    // Check if interactable is hit
-                    // If hit set as focus
+            
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                if (interactable != null)
+                {
+                    SetFocus(interactable);
+                }
 
             }
         }
+    }
+    void SetFocus (Interactable newFocus)
+    {
+        focus = newFocus;
+        motor.FollowTarget(newFocus);
+    }
+    void RemoveFocus()
+    {
+        focus = null;
+        motor.StopFollowingTarget();
     }
 }
--- Game_003/Assets/Scripts/PlayerMotor.cs
+++ Game_003/Assets/Scripts/PlayerMotor.cs
@@ -6,6 +6,7 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class PlayerMotor : MonoBehaviour {
 
+    Transform target;
     NavMeshAgent agent;
 
 	// Use this for initialization
@@ -13,10 +14,26 @@ public class PlayerMotor : MonoBehaviour {
         agent = GetComponent<NavMeshAgent>();
 	}
 
+    private void Update()
+    {
+        if (target != null)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
 
     public void MoveToPoint (Vector3 point)
     {
         agent.SetDestination(point);
     }
-		
+
+    public void FollowTarget(Interactable newTarget)
+    {
+        target = newTarget.transform;
+    }
+    public void StopFollowingTarget()
+    {
+        target = null;
+    }
+
 	}
