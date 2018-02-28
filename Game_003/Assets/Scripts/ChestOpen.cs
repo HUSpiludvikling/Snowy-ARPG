76bb88049649b633e36f344de88dc0d7568aa47c
Unknown <xvictor.victor@gmail.com>
2018-02-28 14:36:19 +0100

    started scripting the ehhh chest
    you can now open and close it. But it's kinda buggy

---
+++ Game_003/Assets/Scripts/ChestOpen.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestOpen : MonoBehaviour {
+    private Animator ChestAnim;
+    private bool inside = false;
+
+    private void Start()
+    {
+        ChestAnim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            inside = true;   
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (inside == true)
+        {
+        ChestAnim.SetBool("close", false);
+        ChestAnim.SetBool("open", true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            ChestAnim.SetBool("open", false);
+            ChestAnim.SetBool("close", true);
+            inside = false;
+        }
+    }
+
+}
