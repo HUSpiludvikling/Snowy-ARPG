73a637810eb178ab961badeb3553c88fc6e1947e
Unknown <jacobaaa@hotmail.dk>
2018-03-12 15:47:20 +0100

    Health Bare V1
    Så har vi en gate keeper.

--- Snowy ARPG/Assets/Scripts/PlayerHealth.cs
+++ Snowy ARPG/Assets/Scripts/PlayerHealth.cs
@@ -6,35 +6,51 @@ using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour {
 
-    public float startingHealth = 100;                            // The amount of health the player starts the game with.
-    public float currentHealth;
-    public float maxhealth { get { return startingHealth; } }                                       // The current health the player has.
+    public float startingHealth = 100;                              // The amount of health the player starts the game with.
+    private float currentHealthField;
+    public float currentHealth { get { return currentHealthField; } set {
+            if(value > maxhealth)
+            {
+                currentHealthField = maxhealth; 
+            }
+            else
+            {
+                currentHealthField = value; 
+            }
+            SetCurrentHealth(currentHealthField);
+        }  }
+    public float maxhealth { get { return startingHealth; } }     // The current health the player has.
     //public Slider healthSlider;                                 // Reference to the UI's health bar.
-    public Image damageImage;                                   // Reference to an image to flash on the screen on being hurt.
+    public Image damageImage;                                     // Reference to an image to flash on the screen on being hurt.
     //public AudioClip deathClip;                                 // The audio clip to play when the player dies.
     //public float flashSpeed = 5f;                               // The speed the damageImage will fade at.
     //public Color flashColour = new Color(1f, 0f, 0f, 0.1f);     // The colour the damageImage is set to, to flash.
     public string SceneLoader;
+    private Text texhField;
 
-    void Awake()
-    {
-        // Set the initial health of the player.
-        currentHealth = startingHealth;
 
-    }
-    void Update()
+
+
+    private void SetCurrentHealth(float currentHp)
     {
         //Debug.Log((currentHealth / startingHealth).ToString("0.0"));
         //healthSlider.value = currentHealth;
         damageImage.fillAmount = (currentHealth / startingHealth);
-        if (currentHealth <= 0)
+        if (currentHp <= 0)
         {
             SceneManager.LoadScene(SceneLoader);
         }
-        else if (currentHealth > maxhealth)
-        {
-            currentHealth = maxhealth;
-        }
+        texhField.text = currentHp.ToString();
+    }
+
+
+
+    void Awake()
+    {
+        // Set the initial health of the player.
         
+        texhField = GetComponentInChildren<Text>();
+        currentHealth = startingHealth;
+        //SetCurrentHealth(10);
     }
 }
