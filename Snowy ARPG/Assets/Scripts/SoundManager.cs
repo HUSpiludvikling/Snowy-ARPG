7a1572110f3e82ed56b114ed67f0b1f275dd4731
Ice Cream <victorskovgaardj@gmail.com>
2018-03-21 17:04:26 +0100

    AudioManager
    yay

---
+++ Snowy ARPG/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+[RequireComponent (typeof(AudioSource))]
+public class SoundManager : MonoBehaviour {
+
+ public List<AudioClip> BackgroundMusic = new List<AudioClip>();
+
+    private AudioSource audioSource;
+    private int currentSong;
+    public bool playRandom;
+
+    public void Start()
+    {
+        PlayRandomSong();
+    }
+
+    private void PlayRandomSong()
+    {
+        currentSong = playRandom ?  Random.Range(0, BackgroundMusic.Count) : (currentSong++ %BackgroundMusic.Count);
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = BackgroundMusic[currentSong];
+        audioSource.Play();
+    }
+
+    public void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            PlayRandomSong();
+        }
+    }
+}
