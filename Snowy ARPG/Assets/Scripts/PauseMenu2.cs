965f67e0aac1e164ad81281a7ecc66d4fa814816
Unknown <jacobaaa@hotmail.dk>
2018-04-25 16:41:33 +0200

    Pause
    Pauser på "Esc"

---
+++ Snowy ARPG/Assets/Scripts/PauseMenu2.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu2 : MonoBehaviour {
+
+    public KeyCode pauseKey = KeyCode.Escape;
+    public GameObject pauseOverlay;
+
+
+    private bool paused = false;
+
+    void Start()
+    {
+       //pauseOverlay.SetActive(false);
+
+    }
+
+    void Update()
+    {
+        PauseHandler();
+        if (Input.GetKeyDown(pauseKey))
+            paused = !paused;
+    }
+
+    private void PauseHandler()
+    {
+            if (!paused)
+            {
+                Time.timeScale = 1.0f;
+
+                pauseOverlay.SetActive(value: paused);
+                //print("Unpaused");
+            }
+            else
+            {
+                Time.timeScale = 0.0f;
+                pauseOverlay.SetActive(paused);
+
+                //print("Paused");
+            }
+        
+    }
+}
